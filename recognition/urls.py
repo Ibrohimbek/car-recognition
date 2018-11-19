024253694beb1057bb6ffff51035a34e0015d54f
Ibrohim Ermatov <ibrohimbek@gmail.com>
2018-11-19 21:12:20 +0500

    Improve home page to use bootstrap and receive a file

--- recognition/urls.py
+++ recognition/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.MainView.as_view(), name='home'),
     path('api/', include('recognition.api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recognition/views.py
+++ recognition/views.py
@@ -1,5 +1,23 @@
-from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render, redirect
+from django.views import View
 
 
-def home(request):
-    return HttpResponse('Welcome to car recognition service!')
+class MainView(View):
+    template = 'recognition/main.html'
+
+    def get(self, request):
+        return render(request, self.template)
+
+    def post(self, request):
+        if not request.FILES.get('photo'):
+            return redirect('home')
+
+        photo = request.FILES.get('photo')
+        fs = FileSystemStorage()
+        filename = fs.save(photo.name, photo)
+
+        context = {
+            'uploaded_file_url': fs.url(filename)
+        }
+        return render(request, self.template, context)
